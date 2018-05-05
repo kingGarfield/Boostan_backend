190a214167604dad049dd0bf54a6e564371d1eb2
ali <seyed.ali.hejazi.35@gmail.com>
2018-05-05 14:40:12 +0430

    clearing some mess in code

---
+++ src/main/java/com/hejazi/boostan/database/studentRepository.java
@@ -0,0 +1,6 @@
+package com.hejazi.boostan.database;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface studentRepository extends CrudRepository<Student , Long> {
+}
--- src/main/java/com/hejazi/boostan/logging_in/logging_in_controller.java
+++ src/main/java/com/hejazi/boostan/logging_in/logging_in_controller.java
@@ -1,14 +1,33 @@
 package com.hejazi.boostan.logging_in;
 
+import com.hejazi.boostan.database.Student;
+import com.hejazi.boostan.database.studentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class logging_in_controller {
+    @Autowired
+    private studentRepository studentRepository;
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
-        String s = new String("greeting");
-        return s;
+        return new String("greeting");
+    }
+
+    @RequestMapping("/signup")
+    public String signup(@RequestParam(name="username", required=true) String username,
+                           @RequestParam(name="password", required=true) String password,
+                           @RequestParam(name="api_key", required=true) String api_key) {
+        if(api_key.equals("123")){
+            Student student = new Student();
+            student.setPassword(password);
+            student.setUsername(username);
+            studentRepository.save(student);
+            return new String("you signed up successfully.");
+        }
+        return new String("your request was rejected.");
     }
 }
