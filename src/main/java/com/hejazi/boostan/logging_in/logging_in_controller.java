a954c391e4ec1546ff5c00c7dd60abd25fda2e42
ali <seyed.ali.hejazi.35@gmail.com>
2018-05-12 22:18:43 +0430

    edit mysql config file

--- src/main/java/com/hejazi/boostan/logging_in/logging_in_controller.java
+++ src/main/java/com/hejazi/boostan/logging_in/logging_in_controller.java
@@ -3,31 +3,53 @@ package com.hejazi.boostan.logging_in;
 import com.hejazi.boostan.database.Student;
 import com.hejazi.boostan.database.studentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class logging_in_controller {
     @Autowired
     private studentRepository studentRepository;
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
-        return new String("greeting");
-    }
+
 
     @RequestMapping("/signup")
     public String signup(@RequestParam(name="username", required=true) String username,
                            @RequestParam(name="password", required=true) String password,
                            @RequestParam(name="api_key", required=true) String api_key) {
-        if(api_key.equals("123")){
+        if(api_key.equals("123isagoodpassword")){
             Student student = new Student();
             student.setPassword(password);
             student.setUsername(username);
+            for (Student thestudent:
+                 studentRepository.findAll()) {
+                if(thestudent.getUsername().equals(username))
+                    return new String("This username is taken.Try another.");
+            }
             studentRepository.save(student);
             return new String("you signed up successfully.");
         }
         return new String("your request was rejected.");
+    }
+
+    @RequestMapping("/signin")
+    public String signin(@RequestParam(name="username", required=true) String username,
+                         @RequestParam(name="password", required=true) String password,
+                         @RequestParam(name="api_key", required=true) String api_key) {
+        if(api_key.equals("123isagoodpassword")){
+            Student student = new Student();
+            student.setPassword(password);
+            student.setUsername(username);
+            for (Student thestudent:
+                    studentRepository.findAll()) {
+                if(thestudent.getUsername().equals(username) && student.getPassword().equals(password))
+                    return new String("signed in successfully.");
+            }
+        }
+        return new String("your username or password is incorrect");
     }
 }
