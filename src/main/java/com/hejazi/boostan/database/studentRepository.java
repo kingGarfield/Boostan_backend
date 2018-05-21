97893890c2e692c013d66737e36876edf063f8dc
ali <seyed.ali.hejazi.35@gmail.com>
2018-05-21 13:56:58 +0430

    clearing some mess

--- src/main/java/com/hejazi/boostan/database/studentRepository.java
+++ src/main/java/com/hejazi/boostan/database/studentRepository.java
@@ -2,5 +2,9 @@ package com.hejazi.boostan.database;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface studentRepository extends CrudRepository<Student , Long> {
+    Optional<Student> findByUsername(String username);
+    Optional<Student> findByPasswordOrderByUsername(Student var1);
 }
