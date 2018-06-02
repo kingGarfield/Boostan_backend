9aab3b394fc952cb908e51dffb2f527c198d91da
ali <seyed.ali.hejazi.35@gmail.com>
2018-06-02 11:47:41 +0430

    clearing up some messes

--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -6,7 +6,7 @@ insert into course VALUES (3,"فیزیک 1","اختصاصی",3);
 insert into course VALUES (4,"مبانی برنامه نویسی","اختصاصی",3);
 
 INSERT INTO student VALUES (1,"خیابان عقیل","1997-04-14 00:00:00","seyed.ali.hejazi.35@gmail.com","سید علی","حجازی",
-                              "0020045875","123nicepass","+989120882280","2018-00-00 00:00:00","kingGarfield");
+                              "0020045875","123456","+989120882280","2018-00-00 00:00:00","9421983");
 
 INSERT INTO student VALUES (2,"مهم نیست.","0000-00-00 00:00:00","ali@ahamadi.sh","ali","ahmadi"
   ,"000000000","123","+9891111111","0000-00-00 00:00:00","bighead");
